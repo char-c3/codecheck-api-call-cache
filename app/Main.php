d3a00866e33bf504ae2460dd3761e46efd21a15f
char_c3 <work238fnct@gmail.com>
2017-06-03 06:39:48 +0900

    エラー処理を全く考えないものを作成

--- app/Main.php
+++ app/Main.php
@@ -1,8 +1,43 @@
 <?php namespace Codecheck;
 
+define("URL", "http://challenge-server.code-check.io/api/recursive/ask");
+$result_cache = [];
+$f_memo = [1, null, 2];
+$seed = "";
+
+function askServer($n) {
+    global $seed;
+    $query = "?n=" . $n . "&seed=" . urlencode($seed);
+    $json = file_get_contents(URL . $query);
+    $data = json_decode($json);
+    return intval($data->result);
+}
+
+function f($n) {
+    global $f_memo, $result_cache;
+    if ($n % 2 === 0) {
+        $sum = 0;
+        foreach ([1,2,3,4] as $i) {
+            $m = $n - $i;
+            if ($m >= 0 && !isset($f_memo[$m])) {
+                $f_memo[$m] = f($m);
+            }
+            $sum = $f_memo[$m];
+        }
+        return sum;
+    } else {
+        if (!isset($result_cache[$n])) {
+            $result_cache[$n] = askServer($n);
+        }
+        return $result_cache[$n];
+    }
+}
+
 function run ($argc, $argv)
 {
-  foreach ($argv as $index=>$value) {
-    printf("argv[%s]: %s\n", $index, $value);
-  }
+    global $seed;
+    $seed = $argv[0];
+    $n = $argv[1];
+    echo f(intval($n)), PHP_EOL;
 }
+
